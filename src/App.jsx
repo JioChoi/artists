a86a60485a2279b0614e0cc3dac6c54f05bf40fb
jiochoi <jiochoi7@gmail.com>
2025-03-05 23:00:22 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import { useCallback, useRef, useState } from 'react'
+import artistsFile from './assets/artists.txt'
+import { useEffect } from 'react'
+import Image from './components/image'
+
+function App() {
+    const [artistsList, setArtistsList] = useState(null);
+    const [selectedArtist, setSelectedArtist] = useState(null);
+    const [preview, setPreview] = useState(false);
+    const [currentList, setCurrentList] = useState([]);
+    const index = useRef(0);
+
+    function add() {
+        setCurrentList([...currentList, ...artistsList.slice(index.current, index.current + 40)]);
+        index.current += 40;
+    }
+
+    useEffect(() => {
+        function onScroll() {
+            if (window.innerHeight + window.scrollY + 200 >= document.body.offsetHeight) {
+                add();
+            }
+        }
+        addEventListener('scroll', onScroll);
+
+        return () => {
+            removeEventListener('scroll', onScroll);
+        }
+    }, [currentList, artistsList]);
+
+    useEffect(() => {
+        if (artistsList) {
+            add();
+        }
+    }, [artistsList]);
+
+    // on load
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(artistsFile);
+            const data = await response.text();
+            const artists = data.split('\n');
+            setArtistsList(artists);
+        })();
+
+        addEventListener('mouseup', () => {
+            setPreview(false);
+        });
+    }, []);
+
+    return (
+        <>
+            <div className="container mx-auto p-4">
+                <h1 className="text-5xl font-regular text-center p-5">Artists</h1>
+
+                <div className="w-full bg-gray-100 mt-4 text-md p-4 rounded-lg shadow-md">
+                    <p>Welcome to the Artists Viewer! This list showcases artists tagged in Danbooru.</p>
+                    <p>Click and hold an image to enlarge it.</p>
+                    <p>Click the copy icon to copy the artist's name to your clipboard.</p>
+                    <p>Scroll down to load more artists.</p>
+                    <br/>
+                    <p>All images are generated using NovelAI Diffusion v4 (full version).</p>
+                    <p>Artist tags were weakened using double brackets ( [[  ]] ) to balance v4's strong artist tag influence.</p>
+                    <br/>
+                    <p>Generation Settings:</p>
+                    <p>Model: nai-diffusion-4-full</p>
+                    <p>Prompt: 1girl, solo, komeiji koishi, touhou, [[artist:<bold>ARTIST_NAME</bold>]], blonde hair, wings, red eyes, rating:general, sky, falling petals, upper body, hand on own chest, hand up, happy, looking at viewer, straight-on, outdoors, petals, sky, no text, volumetric lighting, best quality, very aesthetic, absurdres</p>
+                    <p>Undesired Content: blurry, lowres, error, film grain, scan artifacts, worst quality, bad quality, jpeg artifacts, very displeasing, chromatic aberration, multiple views, logo, too many watermarks, sagging breasts</p>
+                    <p>Resolution: 1024x1024</p>
+                    <p>Steps: 20</p>
+                    <p>Sampler: Euler Ancestral (karras)</p>
+                    <p>Prompt Guidance: 6</p>
+                    <p>Prompt Guidance Rescale: 0</p>
+                    <p>Undesired Content Strength: 0</p>
+                </div>
+
+                <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
+                    {
+                        currentList &&
+                        currentList.map((artist, index) => (
+                            <Image key={index} index={index} artist={artist} onMouseDown={() => {
+                                setSelectedArtist(`/${artist}_1.webp`);
+                                setPreview(true);
+                            }} />
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className={`w-full h-full fixed top-0 left-0 flex justify-center items-center bg-black/50 z-50 transition-opacity duration-150 backdrop-blur-xs
+                    ${preview ? 'opacity-100' : 'opacity-0 pointer-events-none'}
+                `}>
+                <div className="max-w-[90%] max-h-[90%] aspect-square select-none shadow-2xl">
+                    <img src={selectedArtist} alt="selected artist" className="rounded-xl" draggable={false}/>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/image.jsx
@@ -0,0 +1,30 @@
+
+
+
+export default function Image({index, onMouseDown, artist}) {
+    return (
+        <div key={index} className="relative bg-gray-100 text-center text-base rounded-lg shadow-md select-none">
+            <div
+                id={index} className="flex justify-center items-center absolute top-0 left-0 z-50 bg-black/50 w-full aspect-square text-white text-base opacity-0 rounded-lg pointer-events-none"
+            >Copied!</div>
+            <div className="absolute right-1 top-1 z-50 bg-black/50 w-8 h-8 flex justify-center items-center rounded-full lg:hover:brightness-90 hover:cursor-pointer"
+                onClick={() => {
+                    navigator.clipboard.writeText("artist:" + artist);
+                    document.getElementById(index).classList.add('copyAnimation');
+                    setTimeout(() => {
+                        document.getElementById(index).classList.remove('copyAnimation');
+                    }, 1000);
+                }}
+            >
+                <span className="mingcute--copy-line w-6 h-6 text-white"></span>
+            </div>
+            <div className="absolute flex justify-center items-top w-full aspect-square text-white text-base opacity-0 lg:hover:cursor-pointer lg:hover:opacity-100 drop-shadow-md"
+                onMouseDown={onMouseDown}
+            >
+                hold to enlarge
+            </div>
+            <img loading="lazy" src={`/${artist}_1.webp`} alt={artist} className="w-full overflow-hidden rounded-lg rounded-b-none select-none" />
+            {artist}
+        </div>
+    )
+}
